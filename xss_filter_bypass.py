3b39f9db5257c64678cd9d2c58480b24279d1cb5
Andrew Kutuzov <britvill@gmail.com>
2022-07-28 19:57:50 +0300

    Code refactoring

--- xss_filter_bypass.py
+++ xss_filter_bypass.py
@@ -5,25 +5,21 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+import hashlib
+import json
 import os
 import re
 import urllib
 
-from burp import IBurpExtender
-from burp import IHttpListener
+from burp import IBurpExtender, IContextMenuFactory, IHttpListener
 from java.io import PrintWriter
 from java.lang.String import getMethod
-from burp import IContextMenuFactory
-from javax.swing import JMenu
-from javax.swing import JMenuItem
-import hashlib
-import urllib
-import json
+from javax.swing import JMenu, JMenuItem
 
 try:
-  from exceptions_fix import FixBurpExceptions
+    from exceptions_fix import FixBurpExceptions
 except ImportError:
-  pass
+    pass
 
 
 PAYLOADS = [
@@ -61,76 +57,71 @@ PAYLOAD_TAG = '{XSS}'
 
 
 class BurpExtender(IBurpExtender, IHttpListener, IContextMenuFactory):
+    """General class for burp extension."""
 
     def registerExtenderCallbacks(self, callbacks):
+        """Extension registration."""
         self._callbacks = callbacks
         self._helpers = callbacks.getHelpers()
-        callbacks.setExtensionName("XSS Filter Bypass")
-        callbacks.registerHttpListener(self)
-        callbacks.registerContextMenuFactory(self)
+        self._callbacks.setExtensionName("XSS Filter Bypass")
+        self._callbacks.registerHttpListener(self)
+        self._callbacks.registerContextMenuFactory(self)
 
     def createMenuItems(self, invocation):
+        """Menu items creation and send payload in request window."""
         self.menus = []
-        self.mainMenu = JMenu("XSS Filter Bypass")
-        self.menus.append(self.mainMenu)
+        self.main_menu = JMenu("XSS Filter Bypass")
+        self.menus.append(self.main_menu)
         self.invocation = invocation
-
-        menuItem = PAYLOADS
-        for payload in menuItem:
+        menu_items = PAYLOADS
+        # Parse checked payload
+        for payload in menu_items:
             menu = JMenuItem(payload, None,
-                             actionPerformed=lambda
-                                 x: self.requestModify(
-                                 x))
-            self.mainMenu.add(menu)
+                             actionPerformed=lambda x: self.requestModify(x))
+            self.main_menu.add(menu)
         return self.menus if self.menus else None
 
     def requestModify(self, x):
+        """Check request method and paste payload."""
         self.payload = PAYLOAD_TAG+x.getSource().text+PAYLOAD_TAG
-        currentRequest = self.invocation.getSelectedMessages()[0]
-        requestInfo = self._helpers.analyzeRequest(currentRequest)
-        self.headers = list(requestInfo.getHeaders())
-        if (requestInfo.getMethod() == "GET"):
-            body = currentRequest.getRequest()  # return byte[]
-            requestInfo = self._helpers.analyzeRequest(
-                currentRequest)  # returns IResponseInfo
-            paraList = requestInfo.getParameters()
-            new_requestInfo = body
-            white_action = ['action', 'sign']
-            for para in paraList:
-                if para.getType() == 0 and not self.Filter(white_action,
-                                                           para.getName()):
-                    value = para.getValue() + self.payload
-                    key = para.getName()
-                    newPara = self._helpers.buildParameter(key, value,
-                                                           para.getType())
-                    new_requestInfo = self._helpers.updateParameter(
-                        new_requestInfo,
-                        newPara)
-
-            currentRequest.setRequest(new_requestInfo)
-
-        elif (requestInfo.getMethod() == "POST"):
-            bodyBytes = currentRequest.getRequest()[
-                        requestInfo.getBodyOffset():]
-            self.body = self._helpers.bytesToString(bodyBytes)
-            o, n = self.update_body(urllib.unquote(self.body))
+        current_request = self.invocation.getSelectedMessages()[0]
+        request_info = self._helpers.analyzeRequest(current_request)
+        self.headers = list(request_info.getHeaders())
+        # If request method == GET
+        if request_info.getMethod() == "GET":
+            body = current_request.getRequest()
+            request_info = self._helpers.analyzeRequest(
+                current_request)
+            param_list = request_info.getParameters()
+            new_request_info = body
+            for param in param_list:
+                if param.getType() == 0:
+                    value = param.getValue() + self.payload
+                    key = param.getName()
+                    new_param = self._helpers.buildParameter(key, value,
+                                                             param.getType())
+                    new_request_info = self._helpers.updateParameter(
+                        new_request_info,
+                        new_param)
+            current_request.setRequest(new_request_info)
+        # If request method == POST
+        elif request_info.getMethod() == "POST":
+            body_bytes = current_request.getRequest()[
+                        request_info.getBodyOffset():]
+            self.body = self._helpers.bytesToString(body_bytes)
+            o, n = self.updateBody(urllib.unquote(self.body))
             self.body = self.body.replace(o, n)
-            newMessage = self._helpers.buildHttpMessage(self.headers,
-                                                        self.body)
-            currentRequest.setRequest(newMessage)
+            new_message = self._helpers.buildHttpMessage(self.headers,
+                                                         self.body)
+            current_request.setRequest(new_message)
 
-    def Filter(self, white_action, key):
-        return True if ([True for i in white_action if
-                         i in key.lower()]) else False
-
-    def update_body(self, body=""):
+    def updateBody(self, body=""):
+        """Update request body."""
         try:
             o = body
-            white_action = ['submit', 'token', 'code', 'id', 'password']
             for item in self.headers:
-                if (item.startswith(
-                        'Content-Type:') and 'application/json' in item) or body.startswith(
-                        '{"'):
+                if ((item.startswith('Content-Type:') and 'application/json'
+                     in item) or body.startswith('{"')):
                     json_type = 1
                     break
                 else:
@@ -138,16 +129,12 @@ class BurpExtender(IBurpExtender, IHttpListener, IContextMenuFactory):
             if json_type == 0:
                 params = o.split('&')
                 for i in range(len(params)):
-                    if self.Filter(white_action, params[i].split('=')[0]):
-                        continue
                     params[i] = params[i] + self.payload
                 n = '&'.join(params)
                 return o, n
             if json_type == 1:
                 data = json.loads(o)
                 for item in data:
-                    if self.Filter(white_action, item):
-                        continue
                     data[item] = data[item] + self.payload
                 n = json.dumps(data)
                 return o, n
@@ -155,61 +142,71 @@ class BurpExtender(IBurpExtender, IHttpListener, IContextMenuFactory):
             return e
 
     def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
-        if toolFlag == 64 or toolFlag == 16 or toolFlag == 32:
+        """Working with request before send and analyze response."""
+        # Check tool: Scanner == 16, Intruder == 32, Repeater == 64
+        if toolFlag == 16 or toolFlag == 32 or toolFlag == 64:
+            # If Request:
             if messageIsRequest:
                 request = messageInfo.getRequest()
-                analyzedRequest = self._helpers.analyzeRequest(
+                analyzed_request = self._helpers.analyzeRequest(
                     request)
-                headers = analyzedRequest.getHeaders()
-                body = request[analyzedRequest.getBodyOffset():]
+                headers = analyzed_request.getHeaders()
+                body = request[analyzed_request.getBodyOffset():]
+                # If request method == POST
                 if self._helpers.analyzeRequest(
                         messageInfo.getRequest()).getMethod() == 'POST':
                     body_string = body.tostring()
                     self.payload = re.search(r'{XSS}[^{]+{XSS}', body_string)
+                    # If payload not empty
                     if self.payload:
                         new_body_string = body_string.replace(PAYLOAD_TAG, '')
                         new_body = self._helpers.bytesToString(new_body_string)
                         self.payload = self.payload.group(0).replace(
                             PAYLOAD_TAG, ''
                         )
+                        # Set new request
                         messageInfo.setRequest(
                             self._helpers.buildHttpMessage(headers, new_body)
                         )
+                # If request method == GET
                 elif self._helpers.analyzeRequest(
                         messageInfo.getRequest()).getMethod() == 'GET':
-                    parameters = self._helpers.analyzeRequest(
+                    param_list = self._helpers.analyzeRequest(
                         messageInfo.getRequest()).getParameters()
                     request_body = messageInfo.getRequest()
                     new_request = request_body
-                    for parameter in parameters:
-                        value = parameter.getValue()
+                    # Search payload in parameters
+                    for param in param_list:
+                        value = param.getValue()
                         payload_search = re.search(r'{XSS}[^{]+{XSS}', value)
                         if payload_search:
                             value = value.replace(PAYLOAD_TAG, '')
                             self.payload = payload_search.group(0).replace(
                                 PAYLOAD_TAG, ''
                             )
-                            key = parameter.getName()
-                            new_parameter = self._helpers.buildParameter(
-                                key, value, parameter.getType())
+                            key = param.getName()
+                            new_param = self._helpers.buildParameter(
+                                key, value, param.getType())
                             new_request = self._helpers.updateParameter(
                                 new_request,
-                                new_parameter)
+                                new_param)
                     messageInfo.setRequest(new_request)
-
+            # If Response
             if not messageIsRequest and self.payload:
                 response = messageInfo.getResponse()
-                analyzedResponse = self._helpers.analyzeResponse(
+                analyzed_response = self._helpers.analyzeResponse(
                     response)
-                headers = analyzedResponse.getHeaders()
-                body = response[analyzedResponse.getBodyOffset():]
+                headers = analyzed_response.getHeaders()
+                body = response[analyzed_response.getBodyOffset():]
                 body_string = body.tostring()
+                # Search payload in response
                 if body_string.find(urllib.unquote(self.payload)) != -1:
                     new_body_string = body_string.replace(
                         urllib.unquote(self.payload),
                         '<!-- '+PAYLOAD_TAG+' -->'+urllib.unquote(self.payload)
                     )
                     new_body = self._helpers.bytesToString(new_body_string)
+                    # Update body and tag payload
                     messageInfo.setResponse(
                         self._helpers.buildHttpMessage(headers, new_body)
                     )
